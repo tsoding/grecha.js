e44f6bb8413708b4c98fc4c18941f07703773167
rexim <reximkut@gmail.com>
2021-05-17 04:05:54 +0700

    Ship Lorem ipsum with the framework
    I think it's generally useful for testing things, so let's just add it
    to the framework.

--- grecha.js
+++ grecha.js
@@ -1,3 +1,5 @@
+const LOREM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
 function tag(name, ...children) {
     const result = document.createElement(name);
     for (const child of children) {
