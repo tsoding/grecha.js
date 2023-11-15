a323202540415381f4c5878e74ef39a9613130ac
SpcFORK <126259962+SpcFORK@users.noreply.github.com>
2023-11-15 18:29:45 +0000

    Wow.

--- grecha.js
+++ grecha.js
@@ -10,6 +10,9 @@ class Grecha {
         window.LOREM = ElementWrapper.LOREM;
       }()
 
+      static RouterSymbol = Symbol.for("grecha-router");
+      static CoreSymbol = Symbol.for("grecha-core");
+
       /* We create attractive class. */
 
       constructor(name, ...children) {
@@ -84,10 +87,10 @@ class Grecha {
               console.log(w);
               cw.element.innerHTML = w.innerHTML;
               w.innerHTML = '';
-              
+
               // Remove w node from Document.
               w?.remove?.()
-              
+
               return cw.element;
 
             }
@@ -116,7 +119,7 @@ class Grecha {
           link$(href, text) {
             let wrapped = this.wrap$('a')
 
-            if (wrapped && (href||text)) {
+            if (wrapped && (href || text)) {
               wrapped.firstChild.att$('href', href)
               wrapped.firstChild.att$('target', '_blank')
               wrapped.firstChild.att$('rel', 'noopener noreferrer')
@@ -126,7 +129,170 @@ class Grecha {
             }
           },
 
+          absolutePos$(x, y) {
+            cw.element.style.position = 'absolute';
+            cw.element.style.display = 'flex';
+            cw.element.style.flexWrap = 'wrap';
+
+            cw.element.style.left = x;
+            cw.element.style.top = y;
+          },
+
+          resetPos$() {
+            cw.element.style.position = 'static';
+            cw.element.style.display = 'block';
+            cw.element.style.flexWrap = 'nowrap';
+
+            cw.element.style.left = '';
+            cw.element.style.top = '';
+          },
+
+
+          // Register a new attribute for a custom data type
+          data$(key, value) {
+            this.att$(`data-${key}`, value);
+            return this;
+          },
+
+          // Append a child element
+          append$(child) {
+            if (child instanceof ElementWrapper) {
+              this.element.appendChild(child.element);
+            } else if (child instanceof HTMLElement) {
+              this.element.appendChild(child);
+            } else if (typeof child === 'string') {
+              this.element.appendChild(document.createTextNode(child));
+            }
+            return this;
+          },
+
+          // Set ID attribute
+          id$(id) {
+            this.att$('id', id);
+            return this;
+          },
+
+          // Set classes
+          class$(...classes) {
+            this.element.classList.add(...classes);
+            return this;
+          },
+
+          // Set styles directly on an element
+          style$(styleObject) {
+            for (const property in styleObject) {
+              this.element.style[property] = styleObject[property];
+            }
+            return this;
+          },
+
+          // Toggle class on an element
+          toggleClass$(className) {
+            this.element.classList.toggle(className);
+            return this;
+          },
+
+          // Set inner HTML
+          html$(htmlContent) {
+            this.element.innerHTML = htmlContent;
+            return this;
+          },
+
+          // Set inner Text
+          text$(textContent) {
+            this.element.textContent = textContent;
+            return this;
+          },
+
+          // Additional methods to be included in the ElementWrapper class
+
+          // Set a placeholder attribute for an input element
+          placeholder$(placeholderValue) {
+            this.att$('placeholder', placeholderValue);
+            return this;
+          },
+
+          // Set the type attribute for an input or button element
+          type$(typeValue) {
+            this.att$('type', typeValue);
+            return this;
+          },
+
+          // Add an event listener
+          on$(event, handler) {
+            this.element.addEventListener(event, handler);
+            return this;
+          },
+
+          // Remove an event listener
+          off$(event, handler) {
+            this.element.removeEventListener(event, handler);
+            return this;
+          },
+
+          // Perform an action upon pressing the Enter key
+          onEnter$(callback) {
+            this.on$('keydown', (e) => {
+              if (e.key === 'Enter') callback(e);
+            });
+            return this;
+          },
+
+          // Focus the element
+          focus$() {
+            this.element.focus();
+            return this;
+          },
+
+          // Blur the element
+          blur$() {
+            this.element.blur();
+            return this;
+          },
+
+          // Add an attribute for accessibility purposes
+          aria$(key, value) {
+            this.att$(`aria-${key}`, value);
+            return this;
+          },
+
+          // Append multiple children at once
+          appendChildren$(...children) {
+            children.forEach((child) => {
+              this.append$(child);
+            });
+            return this;
+          },
+
+          // Set the disabled attribute on an element
+          disabled$(isDisabled) {
+            if (isDisabled) {
+              this.att$('disabled', 'true');
+            } else {
+              this.element.removeAttribute('disabled');
+            }
+            return this;
+          },
+
+          // Create and append a new child element
+          createElement$(tag, ...children) {
+            this.append$(new ElementWrapper(tag, ...children));
+            return this;
+          },
+
+          // Fetch and display content - Example: Asynchronous operation to load data
+          fetchContent$(url, processContentCallback) {
+            fetch(url)
+              .then(response => response.json())
+              .then(data => processContentCallback(this, data))
+              .catch(error => console.error('Error:', error));
+            return this;
+          },
+
+
         }
+
+
       }
 
       _changeElement(el) {
@@ -173,13 +339,14 @@ class Grecha {
         }
         return select;
       },
-
+      
       // @ Router
       router(routes) {
         const resultWrapper = new ElementWrapper("div");
         const result = resultWrapper.get$();
 
-        const GR_SYM = Symbol.for("grecha-router")
+        const GR_SYM = ElementWrapper.RouterSymbol;
+        const GC_SYM = ElementWrapper.CoreSymbol;
 
         // Expose wrapper for quick access
         window[GR_SYM] = resultWrapper;
@@ -219,6 +386,9 @@ class Grecha {
 
             result.replaceChildren(routes[hashLocation](result));
             currentLocation.value = hashLocation;
+
+            // @ Window Handler
+            window[GC_SYM]?.syncHash?.(hashLocation);
 
             return result;
           },
@@ -263,7 +433,7 @@ class Grecha {
 
         return result;
       },
-
+      
     }
     // ---
 
