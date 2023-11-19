3f7f40a72fb9e537d7367b748b143c6518b0cf16
mikhail synzr <mikhail@synzr.space>
2023-11-19 06:22:02 +0000

    router: added options argument with `route404` field

--- grecha.js
+++ grecha.js
@@ -24,6 +24,7 @@ function tag(name, ...children) {
 }
 
 const MUNDANE_TAGS = ["canvas", "h1", "h2", "h3", "p", "a", "div", "span", "select"];
+
 for (let tagName of MUNDANE_TAGS) {
     window[tagName] = (...children) => tag(tagName, ...children);
 }
@@ -36,7 +37,7 @@ function input(type) {
     return tag("input").att$("type", type);
 }
 
-function router(routes) {
+function router(routes, options) {
     let result = div();
 
     function syncHash() {
@@ -46,18 +47,21 @@ function router(routes) {
         }
 
         if (!(hashLocation in routes)) {
-            // TODO(#2): make the route404 customizable in the router component
-            const route404 = '/404';
+            const route404 = options?.route404 ?? '/404';
             console.assert(route404 in routes);
+
             hashLocation = route404;
         }
 
         result.replaceChildren(routes[hashLocation]());
         return result;
     };
+
     syncHash();
+
     // TODO(#3): there is way to "destroy" an instance of the router to make it remove it's "hashchange" callback
     window.addEventListener("hashchange", syncHash);
+
     result.refresh = syncHash;
 
     return result;
