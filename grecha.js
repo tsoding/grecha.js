271fe2f98ed1db46a99d9caaa8b85004b59b4cbc
homeassistant-hacs-m <140271435+homeassistant-hacs-m@users.noreply.github.com>
2024-01-21 23:05:01 +0200

    No more glory days
    Implement signals

--- grecha.js
+++ grecha.js
@@ -1,69 +1,85 @@
 const LOREM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-function tag(name, ...children) {
-    const result = document.createElement(name);
-    for (const child of children) {
-        if (typeof(child) === 'string') {
-            result.appendChild(document.createTextNode(child));
-        } else {
-            result.appendChild(child);
-        }
+let CURRENT_SCOPE;
+function createSignal(value) {
+  let dependents = new Set;
+  return [
+    () => {
+      if (CURRENT_SCOPE) dependents.add(CURRENT_SCOPE);
+      return value;
+    },
+    update => {
+      value = update;
+      prev = dependents;
+      dependents = new Set();
+      for (const f of prev) try { f() } catch (e) { console.error(e) }
     }
-
-    result.att$ = function(name, value) {
-        this.setAttribute(name, value);
-        return this;
-    };
-
-    result.onclick$ = function(callback) {
-        this.onclick = callback;
-        return this;
-    };
-
-    return result;
+  ]
 }
 
-const MUNDANE_TAGS = ["canvas", "h1", "h2", "h3", "p", "a", "div", "span", "select"];
+function make_node(nodelike) {
+  switch (typeof nodelike) {
+    case 'function': {
+      let node
+      CURRENT_SCOPE = function refresh() {
+        CURRENT_SCOPE = refresh;
+        let update = make_node(nodelike());
+        node.parentNode.replaceChild(update, node);
+        node = update;
+        CURRENT_SCOPE = undefined;
+      } 
+      node = make_node(nodelike());
+      CURRENT_SCOPE = undefined;
+      return node
+    }
+    case 'string':
+    case 'number': 
+      return document.createTextNode(nodelike)
+    default:
+      return nodelike
+  }
+}
+
+function tag(name, ...children) {
+  const result = document.createElement(name);
+  for (const child of children) 
+    result.appendChild(make_node(child))
+
+  result.att$ = function (name, value) {
+    this.setAttribute(name, value);
+    return this;
+  };
+
+  result.onclick$ = function (callback) {
+    this.onclick = callback;
+    return this;
+  };
+
+  return result;
+}
+
+const MUNDANE_TAGS = ["canvas", "h1", "h2", "h3", "p", "a", "div", "span", "select", "b"];
 for (let tagName of MUNDANE_TAGS) {
-    window[tagName] = (...children) => tag(tagName, ...children);
+  window[tagName] = (...children) => tag(tagName, ...children);
 }
 
 function img(src) {
-    return tag("img").att$("src", src);
+  return tag("img").att$("src", src);
 }
 
 function input(type) {
-    return tag("input").att$("type", type);
+  return tag("input").att$("type", type);
 }
 
-function router(routes) {
-    let result = div();
+function routeSignal() {
+  const [route, setRoute] = createSignal();
 
-    function syncHash() {
-        let hashLocation = document.location.hash.split('#')[1];
-        if (!hashLocation) {
-            hashLocation = '/';
-        }
-
-        if (!(hashLocation in routes)) {
-            // TODO(#2): make the route404 customizable in the router component
-            const route404 = '/404';
-
-            console.assert(route404 in routes);
-            hashLocation = route404;
-        }
-
-        result.replaceChildren(routes[hashLocation]());
-
-        return result;
-    };
-
-    syncHash();
-
-    // TODO(#3): there is way to "destroy" an instance of the router to make it remove it's "hashchange" callback
-    window.addEventListener("hashchange", syncHash);
-
-    result.refresh = syncHash;
-
-    return result;
+  function syncHash() {
+    setRoute(document.location.hash.split('#')[1] || '/')
+  };
+  syncHash();
+  
+  // TODO(#3): there is way to "destroy" an instance of the router to make it remove it's "hashchange" callback
+  window.addEventListener("hashchange", syncHash);
+  return route;
 }
