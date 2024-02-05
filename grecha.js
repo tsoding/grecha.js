dc77e061836f2c373710078c5410ed063737cf7f
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2024-02-05 12:50:59 +0100

    make the route404 customizable in the router component

--- grecha.js
+++ grecha.js
@@ -36,34 +36,34 @@ function input(type) {
     return tag("input").att$("type", type);
 }
 
-function router(routes) {
+function router(routes, route404 = "/404") {
+
     let result = div();
 
     function syncHash() {
         let hashLocation = document.location.hash.split('#')[1];
+
         if (!hashLocation) {
             hashLocation = '/';
         }
 
         if (!(hashLocation in routes)) {
-            // TODO(#2): make the route404 customizable in the router component
-            const route404 = '/404';
-
             console.assert(route404 in routes);
             hashLocation = route404;
         }
 
         result.replaceChildren(routes[hashLocation]());
-
         return result;
     };
 
     syncHash();
 
     // TODO(#3): there is way to "destroy" an instance of the router to make it remove it's "hashchange" callback
+
     window.addEventListener("hashchange", syncHash);
 
     result.refresh = syncHash;
 
     return result;
+
 }
